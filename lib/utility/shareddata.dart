2240225dd9e66cb8d619b6f19951c0e2b05a9cc6
Elsayed87 <sayedbaraa2010@gmail.com>
2022-07-17 08:19:51 +0200

    update bloc by using repository class - created _onFetched function in bloc .

---
+++ lib/utility/shareddata.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+final List<Color> colors = <Color>[
+  Colors.red,
+  Colors.blue,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.blueAccent,
+  Colors.pinkAccent,
+  Colors.yellow,
+  Colors.cyanAccent,
+  Colors.teal
+];
